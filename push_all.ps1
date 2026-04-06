2f6d6043726b91a4cc655d82d666e906d2befe14
ADAMJR <adamjf123@gmail.com>
2026-04-06 21:53:56 +0100

    update

---
+++ push_all.ps1
@@ -0,0 +1,10 @@
+﻿$repos = ".", "docs", "frontend", "git-agent", "sessions-api", "users-api"
+
+foreach ($repo in $repos) {
+    Write-Host "Pushing $repo..."
+    cd $repo
+    git add .
+    git commit -m "update" 2>$null
+    git push
+    cd ..
+}

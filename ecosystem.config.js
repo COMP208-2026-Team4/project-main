165f0abb2a02e35a03b67927f19b4ead3ced73c0
ADAMJR <adamjf123@gmail.com>
2026-04-06 20:17:05 +0100

    20:16

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+﻿module.exports = {
+    apps: [
+        {
+            name: "git-agent",
+            cwd: "./git-agent",
+            script: "cmd",
+            args: "/c cargo run",
+            autorestart: false
+        },
+        {
+            name: "users-api",
+            cwd: "./users-api",
+            script: "cmd",
+            args: "/c npm run dev",
+            autorestart: false
+        },
+        {
+            name: "sessions-api",
+            cwd: "./sessions-api",
+            script: "cmd",
+            args: "/c npm run dev",
+            autorestart: false
+        },
+        {
+            name: "rest-api",
+            cwd: "./rest-api",
+            script: "cmd",
+            args: "/c npm run dev",
+            autorestart: false
+        },
+        {
+            name: "frontend",
+            cwd: "./frontend",
+            script: "cmd",
+            args: "/c vite",
+            autorestart: false
+        },
+        {
+            name: "docs",
+            cwd: "./docs",
+            script: "cmd",
+            args: "/c npm run dev",
+            autorestart: false
+        }
+    ]
+};

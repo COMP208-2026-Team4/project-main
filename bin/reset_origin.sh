334e214ac6d43bcd302f135f12af38f63ff3a0f3
ADAMJR <adamjf123@gmail.com>
2026-04-09 21:03:13 +0100

    Fix sync issues with Frontend #2

--- bin/reset_origin.sh
+++ bin/reset_origin.sh
@@ -11,30 +11,28 @@ reset_repo() {
     local repo_path="$1"
     echo "===== $repo_path ====="
 
-    if ! pushd "$repo_path" >/dev/null; then
+    if [[ ! -d "$repo_path" ]]; then
         echo "Failed to open repository."
         HAS_ERRORS=1
         echo
         return
     fi
 
-    if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    if ! git -C "$repo_path" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
         echo "Not a git repository."
         HAS_ERRORS=1
-        popd >/dev/null
         echo
         return
     fi
 
-    if ! git fetch origin; then
+    if ! git -C "$repo_path" fetch origin; then
         HAS_ERRORS=1
     fi
 
-    if ! git reset --hard origin/main; then
+    if ! git -C "$repo_path" reset --hard origin/main; then
         HAS_ERRORS=1
     fi
 
-    popd >/dev/null
     echo
 }
 
